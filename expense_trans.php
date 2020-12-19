13eee9248797f55f85962db7686d30986cdfae52
Ho2 <howard_bb@hotmail.com>
2020-12-19 22:47:13 +0800

    update .navbar.php for navbar

--- expense_trans.php
+++ expense_trans.php
@@ -8,44 +8,7 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light">
-        <a href="#" class="navbar-brand"><img src="./img/logo_i.png" width="50px" height="50px"> POCKETMONEY</a>
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-                <a href="dashboard.php" class="nav-link">Dashboard</a>
-            </li>
-            <li class="nav-item dropdown active">
-                <a href="#" class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown">Transactions</a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                    <a href="income_trans.php" class="dropdown-item">Income Summary</a>
-                    <a href="expense_trans.php" class="dropdown-item">Expenses Summary</a>
-                </div>
-            </li>
-            <li class="nav-item">
-                <a href="#" class="nav-link">Budgets</a>
-            </li>
-            <li class="nav-item">
-                <a href="investment.php" class="nav-link">Investments</a>
-            </li>
-            <li class="nav-item">
-                <a href="#" class="nav-link">Debts</a>
-            </li>
-            <li class="nav-item">
-                <a href="#" class="nav-link">Financial Goals</a>
-            </li>
-            <li class="nav-item">
-                <a href="#" class="nav-link">Reports</a>
-            </li>
-            <li class="nav-item dropdown">
-                <a href="#" class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown">Setting</a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                    <a href="#" class="dropdown-item">Profile</a>
-                    <a href="#" class="dropdown-item">Enquiry</a>
-                    <a href="index.html" class="dropdown-item">Logout</a>
-                </div>
-            </li>
-        </ul>
-    </nav>
+    <?php include(".navbar.php"); ?>
 
     <div class="container-fluid background">
         <div class="container-fluid body">
