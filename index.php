5bd789dd42d50ea0eec5f10fe821effaf3071809
Ho2 <howard_bb@hotmail.com>
2021-01-30 11:43:49 +0800

    index change - Howard

--- index.php
+++ index.php
@@ -148,9 +148,8 @@
                     <hr>
                     <p class="card-text" id="tp_num">TP055278</p>
                     <p class="card-text">
-                        I'm a paragraph.
-                        Click here to add your own text and edit me. It’s easy.
-                        Just click “Edit Text” or double click me to add your own content and make changes to the font.
+                        I'm a Howard. Please use our system, thou maybe its goodn't, but it took half of my life. So yea.
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                     </p>
                 </div>
             </div>
