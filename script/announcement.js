f7dc983b5c034a7ba3b4bbcb1623f7a922d1ee74
nashlim01 <71798913+nashlim01@users.noreply.github.com>
2021-02-04 16:57:55 +0800

    fully functional announcement js

--- script/announcement.js
+++ script/announcement.js
@@ -23,3 +23,54 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+$(document).on("click", ".delete-announcement-anchor", function () {
+  var announceID = $(this).parent().parent().find(".announcementID").val();
+  $("#delete_announcementID").val(announceID);
+});
+
+$(document).on("change", ".delete-announcement-anchor", function () {
+  var announceID = $(this).parent().parent().find(".announcementID").val();
+  $("#delete_announcementID").val(announceID);
+});
+
+$(document).ready(function () {
+  $("body").niceScroll();
+});
+
+window.addEventListener("resize", function () {
+  console.log("helo");
+  $("body").niceScroll().resize();
+});
+
+function validateform(ele) {
+  var id = "#" + ele.id;
+  var announcementDate = $(id).find(".form-announcement_date");
+
+   //validate date
+   if (announcement_date.length) {
+    if (!validDate(announcement_date.val())) {
+    //set the error msg to display
+    announcementDate.parent().find(".error").css("display", "inline-block");
+    //decrease some margin-bottom for the form group
+    announcementDate.parent().css("margin-bottom", "0.2rem");
+    return failValidation("Invalid Date!");
+    } else {
+      announcementDate.parent().find(".error").css("display", "none");
+    //decrease some margin-bottom for the form group
+    announcementDate.parent().css("margin-bottom", "1rem");
+    } 
+   }
+}
+
+$(document).on("click", ".edit-announcement-anchor", function () {
+  var announcementID = $(this).parent().parent().find(".announcementID").val();
+  var announcementDate = $(this).parent().parent().find(".announcement_date").val();
+  var announcementTitle = $(this).parent().parent().find(".title").text();
+  var announcementContent = $(this).parent().parent().find(".content").text();
+
+  $("#edit_announcementID").val(announcementID);
+  $("#edit_announcementDate").val(announcementDate);
+  $("#edit_announcementTitle").val(announcementTitle);
+  $("#edit_announcementContent").val(announcementContent);
+});
